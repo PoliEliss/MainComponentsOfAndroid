80645150eabd9999f4f1413f0a8f0e0371fad609
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-15 13:53:53 +0300

    BaseViewBindingFragment added

---
+++ app/src/main/java/com/rorono/maincomponentsofandroid/utils/BaseViewBindingFragment.kt
@@ -0,0 +1,28 @@
+package com.rorono.maincomponentsofandroid.utils
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+
+open class BaseViewBindingFragment<VB:ViewBinding>(private val inflate: (LayoutInflater, ViewGroup?, Boolean)->VB):BaseFragment() {
+
+    private var _binding:VB?= null
+    val binding
+    get() = _binding
+        ?:throw IllegalStateException("Trying to access binding in ${this.lifecycle.currentState} state")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
