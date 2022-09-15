b1e4f88e7b1a94888a115a5e2e4f1be7b7837467
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-15 13:53:31 +0300

    fun checkPermissionReadContact and getContacts added

--- app/src/main/java/com/rorono/maincomponentsofandroid/screens/ContentProviderFragment.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/screens/ContentProviderFragment.kt
@@ -1,25 +1,64 @@
 package com.rorono.maincomponentsofandroid.screens
 
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.provider.ContactsContract
 import android.view.View
-import android.view.ViewGroup
-import com.rorono.maincomponentsofandroid.R
+import androidx.core.app.ActivityCompat
+import com.rorono.maincomponentsofandroid.Contact
+import com.rorono.maincomponentsofandroid.PhoneNumbersAdapter
+import com.rorono.maincomponentsofandroid.databinding.FragmentContentProviderBinding
+import com.rorono.maincomponentsofandroid.utils.BaseViewBindingFragment
 
 
-class ContentProviderFragment : Fragment() {
+class ContentProviderFragment : BaseViewBindingFragment<FragmentContentProviderBinding>(FragmentContentProviderBinding::inflate) {
+    private var adapter = PhoneNumbersAdapter()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonGetContacts.setOnClickListener {
+            checkPermissionReadContact()
+            binding.recyclerViewContactList.adapter = adapter
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content_provider, container, false)
+    private fun checkPermissionReadContact() {
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                android.Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                Array(1) { android.Manifest.permission.READ_CONTACTS },
+                REQUEST_CODE
+            )
+        } else
+        adapter.submitList(getContacts())
+    }
+
+    private fun getContacts():List<Contact> {
+        val listContacts = mutableListOf<Contact>()
+        val cursor = requireActivity().contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null
+        )
+        cursor?.let {
+            while (cursor.moveToNext()) {
+                val name =
+                    cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                val number =
+                    cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                val contact = Contact(name, number)
+                listContacts.add(contact)
+            }
+        }
+        cursor?.close()
+        return listContacts
+    }
+
+    companion object {
+        const val REQUEST_CODE = 200
     }
 }
