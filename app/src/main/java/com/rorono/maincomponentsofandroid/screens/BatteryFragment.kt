a5e8b61c3ecd91fb2296785495b0e2d20a21acf9
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-16 17:15:22 +0300

    fun checkPhoneChargeLevel, convertDataToString, displayingImage added

--- app/src/main/java/com/rorono/maincomponentsofandroid/screens/BatteryFragment.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/screens/BatteryFragment.kt
@@ -5,6 +5,7 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import com.rorono.maincomponentsofandroid.BroadcastReceiver
+import com.rorono.maincomponentsofandroid.R
 import com.rorono.maincomponentsofandroid.databinding.FragmentBateryBinding
 import com.rorono.maincomponentsofandroid.utils.BaseViewBindingFragment
 
@@ -16,7 +17,28 @@ class BatteryFragment :
         super.onViewCreated(view, savedInstanceState)
         activity?.registerReceiver(broadcastReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
         binding.buttonGetBatteryLevel.setOnClickListener {
-            binding.batteryStatus.text = broadcastReceiver.checkPhoneChargeLevel().toString() + "%"
+            val resultCheckPhoneChargeLevel =
+                checkPhoneChargeLevel(broadcastReceiver = broadcastReceiver)
+            binding.batteryStatus.text = convertDataToString(resultCheckPhoneChargeLevel)
+            displayingImage(resultCheckPhoneChargeLevel)
+        }
+    }
+
+    private fun checkPhoneChargeLevel(broadcastReceiver: BroadcastReceiver): Int {
+        return broadcastReceiver.checkPhoneChargeLevel()
+    }
+
+    private fun <T> convertDataToString(data: T): String {
+        return data.toString() + "%"
+    }
+
+    private fun displayingImage(percent: Int) {
+        binding.imageViewBatteryLevel.visibility = View.VISIBLE
+        when (percent) {
+            in 50..100 -> binding.imageViewBatteryLevel.setImageResource(R.drawable.ic_full_baterry)
+            in 50 downTo 25 -> binding.imageViewBatteryLevel.setImageResource(R.drawable.ic_average_battery_4_bar_24)
+            in 25 downTo 0 -> binding.imageViewBatteryLevel.setImageResource(R.drawable.ic_low_battery_2_bar_24)
         }
     }
 }
+
