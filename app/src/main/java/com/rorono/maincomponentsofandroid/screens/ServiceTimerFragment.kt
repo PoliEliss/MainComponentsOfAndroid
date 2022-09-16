3c41c4b984282cc46dded8969f297393df913c25
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-16 17:18:11 +0300

    fun getButtonRendering added

--- app/src/main/java/com/rorono/maincomponentsofandroid/screens/ServiceTimerFragment.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/screens/ServiceTimerFragment.kt
@@ -4,43 +4,41 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.net.ConnectivityManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.rorono.maincomponentsofandroid.R
 import com.rorono.maincomponentsofandroid.ServiceTimer
 import com.rorono.maincomponentsofandroid.databinding.FragmentServiceTimerBinding
+import com.rorono.maincomponentsofandroid.utils.BaseViewBindingFragment
+import com.rorono.maincomponentsofandroid.viewmodel.MainViewModel
+import com.rorono.maincomponentsofandroid.viewmodel.MainViewModelFactory
 import kotlin.math.roundToInt
 
 
-class ServiceTimerFragment : Fragment() {
+class ServiceTimerFragment : BaseViewBindingFragment<FragmentServiceTimerBinding>(
+    FragmentServiceTimerBinding::inflate
+) {
 
-    private val broadcastReceiver = com.rorono.maincomponentsofandroid.BroadcastReceiver()
+    private lateinit var viewModel: MainViewModel
 
     private var time = 0.0
-    private var timeStarted = false
-
-    private var _binding: FragmentServiceTimerBinding? = null
-    private val binding get() = _binding!!
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentServiceTimerBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModelFactory = MainViewModelFactory()
+        viewModel =
+            ViewModelProvider(requireActivity(), viewModelFactory)[MainViewModel::class.java]
+
+        getButtonRendering(viewModel.stateTimer.value as Boolean)
+
+
+        viewModel.stateTimer.observe(viewLifecycleOwner) {
+            getButtonRendering(it)
+        }
         binding.buttonStartAndStopTimer.setOnClickListener {
             startAndStopTimer()
         }
@@ -48,8 +46,8 @@ class ServiceTimerFragment : Fragment() {
             resetTimer()
             binding.buttonResetTimer.visibility = View.GONE
         }
-
         activity?.registerReceiver(updateTime, IntentFilter(ServiceTimer.TIMER_UPDATE))
+
     }
 
     private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
@@ -57,8 +55,8 @@ class ServiceTimerFragment : Fragment() {
             time = intent?.getDoubleExtra(ServiceTimer.EXTRA_TIMER, 0.0) ?: 0.0
             binding.tvTimer.text = getTimerStringFromDouble(time)
         }
-
     }
+
 
     private fun resetTimer() {
         stopTimer()
@@ -68,7 +66,7 @@ class ServiceTimerFragment : Fragment() {
 
 
     private fun startAndStopTimer() {
-        if (timeStarted) {
+        if (viewModel.stateTimer.value as Boolean) {
             stopTimer()
         } else
             startTimer()
@@ -79,9 +77,6 @@ class ServiceTimerFragment : Fragment() {
         val hours = resultInt % 86400 / 3600
         val minutes = resultInt % 86400 % 3600 / 60
         val seconds = resultInt % 86400 % 3600 % 60
-        Log.d("TEST", "hours $hours")
-        Log.d("TEST", "minutes $minutes")
-        Log.d("TEST", "seconds $seconds")
         return makeTimeString(hours, minutes, seconds)
 
     }
@@ -93,26 +88,25 @@ class ServiceTimerFragment : Fragment() {
 
     private fun startTimer() {
         activity?.startService(ServiceTimer.newIntentTimer(requireContext(), time))
-        binding.buttonStartAndStopTimer.setImageResource(R.drawable.ic_pause_24)
-        binding.buttonStartAndStopTimer.setBackgroundResource(R.drawable.custom_button_pause)
-        binding.buttonResetTimer.visibility = View.VISIBLE
-        timeStarted = true
+        viewModel.getStatFun(true)
     }
 
     private fun stopTimer() {
         activity?.stopService(ServiceTimer.newIntentTimer(requireContext(), time))
-        binding.buttonStartAndStopTimer.setImageResource(R.drawable.ic__play_arrow_24)
-        binding.buttonStartAndStopTimer.setBackgroundResource(R.drawable.custom_button_start)
-        timeStarted = false
+        viewModel.getStatFun(false)
     }
 
-    override fun onStart() {
-        super.onStart()
-        val intentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+    private fun getButtonRendering(timeStarted: Boolean) {
+        when (timeStarted) {
+            false -> {
+                binding.buttonStartAndStopTimer.setImageResource(R.drawable.ic__play_arrow_24)
+                binding.buttonStartAndStopTimer.setBackgroundResource(R.drawable.custom_button_start)
+            }
+            true -> {
+                binding.buttonStartAndStopTimer.setImageResource(R.drawable.ic_pause_24)
+                binding.buttonStartAndStopTimer.setBackgroundResource(R.drawable.custom_button_pause)
+                binding.buttonResetTimer.visibility = View.VISIBLE
+            }
+        }
     }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
 }
