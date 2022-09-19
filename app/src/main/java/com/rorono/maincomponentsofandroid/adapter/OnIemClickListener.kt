61ca8ea0fdf22b4322a40375e376ca96ffed7746
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-19 17:34:23 +0300

    OnItemClickListener added

---
+++ app/src/main/java/com/rorono/maincomponentsofandroid/adapter/OnIemClickListener.kt
@@ -0,0 +1,7 @@
+package com.rorono.maincomponentsofandroid.adapter
+
+import com.rorono.maincomponentsofandroid.Contact
+
+interface OnIemClickListener {
+    fun onItemClickCall(contact: Contact)
+}
