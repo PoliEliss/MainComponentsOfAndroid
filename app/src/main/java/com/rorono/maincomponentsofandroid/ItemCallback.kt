134029545f63cc09c38c2e44c4073c25801e058b
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-22 14:07:06 +0300

    change import models

--- app/src/main/java/com/rorono/maincomponentsofandroid/ItemCallback.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/ItemCallback.kt
@@ -1,6 +1,7 @@
 package com.rorono.maincomponentsofandroid
 
 import androidx.recyclerview.widget.DiffUtil
+import com.rorono.maincomponentsofandroid.models.Contact
 
 class ItemCallback : DiffUtil.ItemCallback<Contact>() {
     override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
