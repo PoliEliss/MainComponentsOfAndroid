5a8be780331bdce1b3d4c5ba7b215e0b6c7fbd07
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-22 13:01:31 +0300

    delete fun log

--- app/src/main/java/com/rorono/maincomponentsofandroid/ServiceTimer.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/ServiceTimer.kt
@@ -4,41 +4,25 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder
-import android.util.Log
-import kotlinx.coroutines.*
 import java.util.*
-import javax.xml.transform.Source
 
 
 class ServiceTimer : Service() {
     private val timer = Timer()
-    override fun onCreate() {
-        super.onCreate()
-        log("onCreate")
-    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        log("onStartCommand")
         val time = intent?.getDoubleExtra(EXTRA_TIMER, 0.0) ?: 0.0
         timer.scheduleAtFixedRate(TimeTask(time), 0, 1000)
-
         return START_NOT_STICKY
     }
 
-
-
     override fun onDestroy() {
         super.onDestroy()
-        log("onDestroy")
         timer.cancel()
     }
 
     override fun onBind(p0: Intent?): IBinder? {
         TODO("Not yet implemented")
-    }
-
-    private fun log(message: String) {
-        Log.d("SERVICE", "Service: $message")
     }
 
     private inner class TimeTask(private var time: Double) : TimerTask() {
@@ -48,7 +32,6 @@ class ServiceTimer : Service() {
             intent.putExtra(EXTRA_TIMER,time)
             sendBroadcast(intent)
         }
-
     }
 
     companion object {
