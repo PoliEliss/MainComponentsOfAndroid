a8955fd0e9c944475755ed3d360db7b99e02531f
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-22 14:07:00 +0300

    change import models

--- app/src/main/java/com/rorono/maincomponentsofandroid/adapter/OnIemClickListener.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/adapter/OnIemClickListener.kt
@@ -1,6 +1,6 @@
 package com.rorono.maincomponentsofandroid.adapter
 
-import com.rorono.maincomponentsofandroid.Contact
+import com.rorono.maincomponentsofandroid.models.Contact
 
 interface OnIemClickListener {
     fun onItemClickCall(contact: Contact)
