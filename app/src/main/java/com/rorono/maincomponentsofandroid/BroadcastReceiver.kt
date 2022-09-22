68a9e9b9a857586a0490dd6fc2a451f9592d1d71
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-22 12:59:52 +0300

    intent.action added

--- app/src/main/java/com/rorono/maincomponentsofandroid/BroadcastReceiver.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/BroadcastReceiver.kt
@@ -3,18 +3,13 @@ package com.rorono.maincomponentsofandroid
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
 import android.os.BatteryManager
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.rorono.maincomponentsofandroid.screens.ContentProviderFragment
 
 class BroadcastReceiver : BroadcastReceiver() {
-    var batteryLevel: Int = 0
+    private var batteryLevel: Int = 0
 
     override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.action
         val level = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
         val scale = intent?.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
 
