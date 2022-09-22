30949af902bddc79a07de6290e00bb094c9ecb96
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-22 11:08:44 +0300

    fun getTimerStringFromDouble makeTiMeString added

--- app/src/main/java/com/rorono/maincomponentsofandroid/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/viewmodel/MainViewModel.kt
@@ -4,13 +4,27 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.math.roundToInt
 
 class MainViewModel() : ViewModel() {
 
     val stateTimer = MutableLiveData<Boolean>(false)
 
+    val time = MutableLiveData<String>()
+
     fun getStatFun(state: Boolean) {
         stateTimer.value = state
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int) {
+        time.value = String.format("%02d:%02d:%02d", hour, min, sec)
+    }
+     fun getTimerStringFromDouble(time: Double) {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+     makeTimeString(hours, minutes, seconds)
     }
 
 }
