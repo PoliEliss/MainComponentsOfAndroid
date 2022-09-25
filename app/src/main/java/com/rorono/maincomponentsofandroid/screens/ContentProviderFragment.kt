51d02132d92237118a97b6ecc8da8d725de17242
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-25 14:47:27 +0300

    avatar_uri added

--- app/src/main/java/com/rorono/maincomponentsofandroid/screens/ContentProviderFragment.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/screens/ContentProviderFragment.kt
@@ -1,45 +1,66 @@
 package com.rorono.maincomponentsofandroid.screens
 
+import android.content.ContentUris
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import com.rorono.maincomponentsofandroid.models.Contact
 import com.rorono.maincomponentsofandroid.R
 import com.rorono.maincomponentsofandroid.adapter.OnIemClickListener
 import com.rorono.maincomponentsofandroid.adapter.PhoneNumbersAdapter
 import com.rorono.maincomponentsofandroid.databinding.FragmentContentProviderBinding
+import com.rorono.maincomponentsofandroid.models.Contact
 import com.rorono.maincomponentsofandroid.utils.BaseViewBindingFragment
+import com.rorono.maincomponentsofandroid.viewmodel.MainViewModel
+import com.rorono.maincomponentsofandroid.viewmodel.MainViewModelFactory
 
 
 class ContentProviderFragment :
     BaseViewBindingFragment<FragmentContentProviderBinding>(FragmentContentProviderBinding::inflate) {
     private var adapter = PhoneNumbersAdapter()
 
+    private lateinit var viewModel: MainViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModelFactory = MainViewModelFactory()
+        viewModel =
+            ViewModelProvider(requireActivity(), viewModelFactory)[MainViewModel::class.java]
         binding.recyclerViewContactList.adapter = adapter
+
+        viewModel.listContact.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
         binding.buttonGetContacts.setOnClickListener {
             checkPermissionReadContact()
         }
 
         binding.buttonWriteContacts.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_contentProviderFragment_to_addContactNumberFragment)
+            Navigation.findNavController(view)
+                .navigate(R.id.action_contentProviderFragment_to_addContactNumberFragment)
         }
         adapter.setOnListener(object : OnIemClickListener {
             override fun onItemClickCall(contact: Contact) {
-                val intent = Intent(Intent.ACTION_DIAL, Uri.parse(getString(R.string.tel) + contact.tel))
+                val intent =
+                    Intent(Intent.ACTION_DIAL, Uri.parse(getString(R.string.tel) + contact.tel))
                 startActivity(intent)
             }
         })
     }
 
     private fun checkPermissionReadContact() {
-        if (ActivityCompat.checkSelfPermission(
+        if (ContextCompat.checkSelfPermission(
                 requireActivity(),
                 android.Manifest.permission.READ_CONTACTS
             ) != PackageManager.PERMISSION_GRANTED
@@ -50,27 +71,48 @@ class ContentProviderFragment :
                 REQUEST_CODE_READ_CONTACTS
             )
         } else
-            adapter.submitList(getContacts())
+            getContacts()
     }
 
-    private fun getContacts(): List<Contact> {
+    private fun getContacts() {
         val listContacts = mutableListOf<Contact>()
         val cursor = requireActivity().contentResolver.query(
             ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null
         )
         cursor?.let {
+            var avatar: Bitmap? = null
+            Log.d("TEST","it ${it.count}")
             while (it.moveToNext()) {
                 val name =
                     it.getString(it.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                Log.d("TEST","name $name")
+
                 val number =
                     it.getString(it.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                val contact = Contact(name, number)
+                /*val type = it.getInt(it.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.TYPE))
+                if (type == ContactsContract.CommonDataKinds.Phone.TYPE_HOME){
+                    Log.d("TEST","Home type ${number} $type")
+                }*/
+
+
+                val avatar_uri = it.getString(it.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.PHOTO_URI))
+
+                if (avatar_uri !=null){
+                    avatar = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        ImageDecoder.decodeBitmap( ImageDecoder.createSource(requireActivity().contentResolver,Uri.parse(avatar_uri)))
+                    } else {
+                        MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,Uri.parse(avatar_uri))
+                    }
+                }
+                val contact = Contact(if (name == number) "No name" else name, number, avatarId = avatar)
                 listContacts.add(contact)
+                avatar = null
             }
         }
         cursor?.close()
-        return listContacts
+         viewModel.getContacts(listContacts)
     }
+
     companion object {
         const val REQUEST_CODE_READ_CONTACTS = 200
     }
