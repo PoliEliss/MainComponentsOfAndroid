863344b1ba5824583427cc2e8b472eff04bfafc5
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-25 14:42:24 +0300

    LiveData added

--- app/src/main/java/com/rorono/maincomponentsofandroid/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/viewmodel/MainViewModel.kt
@@ -1,26 +1,43 @@
 package com.rorono.maincomponentsofandroid.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.rorono.maincomponentsofandroid.models.Contact
 import kotlin.math.roundToInt
 
 class MainViewModel : ViewModel() {
 
-    val stateTimer = MutableLiveData(false)
-    val time = MutableLiveData<String>()
+    private val _stateTimer = MutableLiveData(false)
+    val stateTimer: LiveData<Boolean>
+        get() = _stateTimer
 
-    fun getStatFun(state: Boolean) {
-        stateTimer.value = state
+    private val _time = MutableLiveData<String>()
+    val time: LiveData<String>
+        get() = _time
+
+    fun getStateTimer(state: Boolean) {
+        _stateTimer.value = state
     }
+
+    private val _listContacts = MutableLiveData<List<Contact>>()
+    val listContact: LiveData<List<Contact>>
+        get() = _listContacts
+
 
     private fun makeTimeString(hour: Int, min: Int, sec: Int) {
-        time.value = String.format("%02d:%02d:%02d", hour, min, sec)
+        _time.value = String.format("%02d:%02d:%02d", hour, min, sec)
     }
-     fun getTimerStringFromDouble(time: Double) {
+
+    fun getTimerStringFromDouble(time: Double) {
         val resultInt = time.roundToInt()
         val hours = resultInt % 86400 / 3600
         val minutes = resultInt % 86400 % 3600 / 60
         val seconds = resultInt % 86400 % 3600 % 60
-     makeTimeString(hours, minutes, seconds)
+        makeTimeString(hours, minutes, seconds)
+    }
+
+    fun getContacts(listContacts: MutableList<Contact>) {
+        _listContacts.value = listContacts
     }
 }
