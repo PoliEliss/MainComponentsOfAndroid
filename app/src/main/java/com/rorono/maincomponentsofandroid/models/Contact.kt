7a25374386eaf1ef03b7b15975966d75380bbdf7
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-27 13:53:30 +0300

    changing class fields

--- app/src/main/java/com/rorono/maincomponentsofandroid/models/Contact.kt
+++ app/src/main/java/com/rorono/maincomponentsofandroid/models/Contact.kt
@@ -2,4 +2,4 @@ package com.rorono.maincomponentsofandroid.models
 
 import android.graphics.Bitmap
 
-data class Contact(val name: String?, val tel: String?, val avatarId: Bitmap?)
+data class Contact(val name: String?, var tel: String?, val avatarId: Bitmap?,var homeNumber:String?, val id:String)
